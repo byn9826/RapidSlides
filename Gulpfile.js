10179dbf451a7c72f27ccd3391cfe896c27dfbd5
Yinan Bao <byn9826@gmail.com>
2017-07-28 21:16:00 -0400

    move to sass

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var input = './static/css/*.scss';
+var output = './static/css/';
+
+gulp.task('sass', function () {
+  return gulp
+    .src(input)
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest(output));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch(input, ['sass']);
+});
