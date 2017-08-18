92f349b7f55119bce38f1cddee2c6d23202b1899
Yinan Bao <byn9826@gmail.com>
2017-08-17 21:35:36 -0400

    list all slides in start page

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,35 @@
 const fs = require( 'fs' );
 const path = require( 'path' );
-var files = [];
+const util = require( 'util' );
+const { remote } = require('electron')
 
 window.onload = function() {
 
-    const main = document.getElementById( "main" );
+    const main = document.getElementById( "main-container" );
     
     fs.readdirSync( path.join( __dirname, './workspace/' )).forEach( file => {
         if ( file.split( "." ).pop() === "html" || file.split( "." ).pop() === "HTML" ) {
-            files.push( file );
+            
+            let stats = fs.statSync( path.join( __dirname, './workspace/' + file ) );
+            let lTime = new Date( util.inspect( stats.mtime ) );
+
             let title = document.createElement( "div" );
             let titleText = document.createTextNode( file.split( "." )[ 0 ] );
             title.appendChild( titleText );
-            main.appendChild( title ); 
+
+            let created = document.createElement( "span" );
+            let createdText = document.createTextNode( lTime.toString().substring( 4, 24 ) );
+            created.appendChild( createdText );
+
+            let section = document.createElement( "section" );
+            section.appendChild( title );
+            section.appendChild( created );
+            section.onclick = function() { loadFile( file ); }
+            main.appendChild( section ); 
         }
     });
-    console.log(files)
 }
 
-
-
-
+function loadFile( file ) {
+    remote.getCurrentWindow().loadURL( path.join( __dirname, './workspace/', file ) );
+}
